7808b5d10882c2a381f56c4f83f7c803d56ab092
Luke Campbell <luke.s.campbell@gmail.com>
2015-10-05 12:27:15 -0400

    Improves performance of get location

--- cbibs_api/queries/GetMetaDataLocation.sql
+++ cbibs_api/queries/GetMetaDataLocation.sql
@@ -2,17 +2,11 @@
 SELECT
     l.latitude,
     l.longitude
-FROM cbibs.f_observation o
-JOIN cbibs.d_location l on l.id = o.d_location_id
-JOIN cbibs.d_location_type lt on lt.id = o.d_location_type_id
-JOIN cbibs.d_station s ON s.id = o.d_station_id
+FROM cbibs.d_station s
+JOIN cbibs.d_location l on l.id = s.d_location_id
 JOIN cbibs.d_provider pr ON pr.id = s.d_provider_id
-JOIN cbibs.d_qa_code_primary qc ON qc.id = o.d_qa_code_primary_id
 WHERE
-    UPPER(pr.organization) = UPPER(%(constellation)s)
-    AND s.description = %(station)s
-    AND lt.location_type = 'Observed Latitude, Longitude, Station Elevation'
-    AND qc.qa_code = 1
-ORDER BY measure_ts
+    UPPER(pr.organization) = UPPER('cbibs')
+    AND s.description = 'J'
 LIMIT 1;
 
